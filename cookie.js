638aa23f7491776da836f2c4466119605a22b7dc
slayerSahara <sjones@student.neumont.edu>
2017-06-06 20:51:30 -0600

    More changes (Sarah)
    Sessions shtuff

--- cookie.js
+++ cookie.js
@@ -20,4 +20,40 @@ app.get('/clear', function (req, res) {
     res.redirect('/');
 });
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie(){
+    var user = getCookie("username");
+
+    if(user != ""){
+        console.log("Welcome, " + user);
+    }
+    else {
+        setCookie("username", user, 365);
+    }
+}
+
+checkCookie();
+
 app.listen(3000);
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var express = require('express'),
+var expressSession = require('express-session');
+var cookieParser = require('cookie-parser');
   pug = require('pug'),
   path = require('path'),
   route = require('./routes/routes.js'),
   bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(cookieParser());
+app.use(expressSession({secret: 'Secrets and stuff', saveUninitialized: true, resave: true}));
+
+// app.get('/', function (req, res) {
+//     req.session.name = req.session.name || new Date().toUTCString();
+//     console.log(req.sessionID);
+//     res.send(req.session.name);
+// });
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
